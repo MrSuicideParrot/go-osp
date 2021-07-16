9e5f28527a2e4058fb25a7280ea7a8078b37e8b1
André Cirne <dumahk21@gmail.com>
2021-07-16 10:39:44 +0100

    Create independent struct for results

--- osp/messages/getscan.go
+++ osp/messages/getscan.go
@@ -6,36 +6,34 @@ type GetScansRequest struct {
 	XMLName xml.Name `xml:"get_scans"`
 	Text    string   `xml:",chardata"`
 	ScanID  string   `xml:"scan_id,attr"`
-	Details bool   `xml:"details,attr"`
+	Details bool     `xml:"details,attr"`
+}
+
+type Result struct {
+	Text     string  `xml:",chardata"`
+	Host     string  `xml:"host,attr"`
+	Severity float32 `xml:"severity,attr"`
+	Port     string  `xml:"port,attr"`
+	TestID   string  `xml:"test_id,attr"`
+	Name     string  `xml:"name,attr"`
+	Type     string  `xml:"type,attr"`
 }
 
 type GetScansResponse struct {
 	XMLName    xml.Name `xml:"get_scans_response"`
 	Text       string   `xml:",chardata"`
 	StatusText string   `xml:"status_text,attr"`
-	Status     int   `xml:"status,attr"`
+	Status     int      `xml:"status,attr"`
 	Scan       struct {
-		Text      string `xml:",chardata"`
-		ID        string `xml:"id,attr"`
-		Target    string `xml:"target,attr"`
-		EndTime   string `xml:"end_time,attr"`
+		Text      string  `xml:",chardata"`
+		ID        string  `xml:"id,attr"`
+		Target    string  `xml:"target,attr"`
+		EndTime   string  `xml:"end_time,attr"`
 		Progress  float32 `xml:"progress,attr"`
-		StartTime string `xml:"start_time,attr"`
+		StartTime string  `xml:"start_time,attr"`
 		Results   struct {
-			Text   string `xml:",chardata"`
-			Result []struct {
-				Text     string `xml:",chardata"`
-				Host     string `xml:"host,attr"`
-				Severity float32 `xml:"severity,attr"`
-				Port     string `xml:"port,attr"`
-				TestID   string `xml:"test_id,attr"`
-				Name     string `xml:"name,attr"`
-				Type     string `xml:"type,attr"`
-			} `xml:"result"`
+			Text   string   `xml:",chardata"`
+			Result []Result `xml:"result"`
 		} `xml:"results"`
 	} `xml:"scan"`
 }
-
-
-
-
